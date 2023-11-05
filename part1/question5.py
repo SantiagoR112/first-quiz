d6a98407ab16eaea3fef3cdf56137439c5fc58da
SantiagoR112 <u20191177634@usco.edu.co>
2023-11-04 23:01:07 -0500

    Question5

--- part1/question5.py
+++ part1/question5.py
@@ -20,7 +20,11 @@
 
 sql_create_favorite_foods = """
 
-Your SQL here.
+CREATE TABLE favorite_foods (
+    food_id int NOT NULL PRIMARY KEY,
+    name text NOT NULL,
+    vegetarian int
+);
 
 """
 
@@ -30,7 +34,11 @@ Your SQL here.
 
 sql_alter_tables_with_favorite_food = """
 
-Your SQL here.
+ALTER TABLE animals 
+ADD favorite_food_id int;
+
+ALTER TABLE people 
+ADD favorite_food_id int;
 
 """
 
@@ -40,6 +48,9 @@ Your SQL here.
 
 sql_select_all_vegetarian_pets = """
 
-Your SQL here.
+SELECT animals.name, favorite_foods.name 
+FROM animals
+JOIN favorite_foods ON animals.favorite_food_id = favorite_foods.food_id
+WHERE favorite_foods.vegetarian = 1; 
 
 """
